4ccaa1708abbebae5748cc98588f9f126804e959
victormc13 <viktorsb14@gmail.com>
2024-01-06 09:00:56 -0800

    update: State inherits from BaseModel and Base (respecting the order) that represents states table

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+# Retrieve the value of the environment variable 'STORAGE_TYPE'
+storage_type = os.getenv('HBNB_TYPE_STORAGE', 'file')
+
+
+if storage_type == 'db':
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+
+        cities = relationship(
+                "City", back_populates="state", cascade="all, delete-orphan")
+else:
+    class State(BaseModel):
+        @property
+        def cities(self):
+            """Getter attribute that returns a list of City instances
+        with state_id equals to the current State.id"""
+            from models import storage
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
