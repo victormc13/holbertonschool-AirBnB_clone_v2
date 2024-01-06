cf658de9a86ce02738d5a02a816f468a7ade89c5
victormc13 <viktorsb14@gmail.com>
2024-01-06 13:30:31 -0800

    update: Fix pycodestyle, expected an indented block (comment)

--- console.py
+++ console.py
@@ -231,12 +231,12 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
                 return
             for k, v in storage._FileStorage__objects.items():
-            # for k, v in storage.all(HBNBCommand.classes[args]).items():
+                # for k, v in storage.all(HBNBCommand.classes[args]).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
             for k, v in storage._FileStorage__objects.items():
-            # for k, v in storage.all:
+                # for k, v in storage.all:
                 print_list.append(str(v))
 
         print(print_list)
