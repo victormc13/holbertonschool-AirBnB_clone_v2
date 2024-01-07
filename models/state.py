d8356ce6b2bdad20778a1768bafa7b0049b465df
iShezmu <arteaga25610@gmail.com>
2024-01-07 18:56:13 -0500

    update: import 'City' class and can list all 'state' in MYSQL

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
@@ -14,6 +15,7 @@ if storage_type == 'db':
     class State(BaseModel, Base):
         """ State class """
         __tablename__ = 'states'
+        id = Column(String(60), nullable=False, primary_key=True)
         name = Column(String(128), nullable=False)
 
         cities = relationship(
