d4afbabf4f2eb51bff8c8a5c3fbceb1c08fe6bb7
iShezmu <arteaga25610@gmail.com>
2024-01-07 21:22:21 -0500

    update: 'Place' adding an instance of SQLAlchemy Table called 'place_amenity' and updating 'Place' class.

--- models/place.py
+++ models/place.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+
+# Define the association table for the Many-To-Many relationship
+place_amenity = Table('place_amenity', Base.metadata,
+                     Column('place_id', String(60), ForeignKey('places.id'),
+                            primary_key=True, nullable=False),
+                     Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                            primary_key=True, nullable=False)
+                     )
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -18,9 +27,30 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
     
-    # Add a relationship to City and User
+    # Add a relationship to City and User, and Review
     city = relationship("City", back_populates="places")
     user = relationship("User", back_populates="places")
     reviews = relationship("Review", back_populates="place", cascade="all, delete-orphan")
+    
+    # Define the relationship with Amenity as secondary to place_amenity
+    amenities = relationship("Amenity", secondary=place_amenity, back_populates="place_amenities", viewonly=False)
+
+    # Getter attribute for amenities
+    @property
+    def amenities(self):
+        """Getter attribute that returns the list of Amenity instances based on amenity_ids"""
+        from models import storage
+        amenity_list = []
+        for amenity_id in self.amenity_ids:
+            amenity = storage.get("Amenity", amenity_id)
+            if amenity:
+                amenity_list.append(amenity)
+        return amenity_list
+
+    # Setter attribute for amenities
+    @amenities.setter
+    def amenities(self, amenity):
+        """Setter attribute that handles append method for adding an Amenity.id to amenity_ids"""
+        if amenity.__class__.__name__ == "Amenity":
+            self.amenity_ids.append(amenity.id)
