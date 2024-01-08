30c428368bab9dad9fb69a698e7bfd937c5b233f
iShezmu <arteaga25610@gmail.com>
2024-01-07 20:26:09 -0500

    update: Add a relationship to 'Place' with cascade deletion

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -12,3 +13,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    
+    # Add a relationship to Place with cascade deletion
+    places = relationship("Place", back_populates="user", cascade="all, delete-orphan")
