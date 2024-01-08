0e8c944e2edc57a7a0973900ed3aa0f6d08fd42f
iShezmu <arteaga25610@gmail.com>
2024-01-07 19:01:50 -0500

    update: Can list now all 'City' in MySQL

--- models/city.py
+++ models/city.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
+    id: Column(String(60), nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
