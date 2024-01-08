bb56238751ffba60a5996375bce359fa0473811a
iShezmu <arteaga25610@gmail.com>
2024-01-07 21:05:45 -0500

    update: 'Review' inherits from 'BaseModel' and 'Base', and updating classes attributes.

--- models/review.py
+++ models/review.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    
+    text = Column(String(1024), nullable=False)
+
+    # ForeignKey to places.id
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+
+    # ForeignKey to users.id
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new Review"""
+        super().__init__(*args, **kwargs)
