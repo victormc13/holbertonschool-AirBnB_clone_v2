9e3677dc5f29ef98b3e957915c164ffd062cc947
iShezmu <arteaga25610@gmail.com>
2024-01-07 19:00:41 -0500

    test: 'State' object has now attribute 'name'

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ """
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
@@ -15,5 +16,7 @@ class test_state(test_basemodel):
 
     def test_name3(self):
         """ """
-        new = self.value()
+        # new = self.value()
+        new = State()
+        self.assertTrue(hasattr(new, 'name'))
         self.assertEqual(type(new.name), str)
