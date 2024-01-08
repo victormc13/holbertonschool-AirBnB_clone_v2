d225c761371e9c5e900d666a9da5115f76d9d626
iShezmu <arteaga25610@gmail.com>
2024-01-07 21:21:18 -0500

    update: 'Amenity' inherits from 'BaseModel' and 'Base', updating classes attributes.

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+# Define the association table for the Many-To-Many relationship
+association_table = Table('place_amenity', BaseModel.metadata,
+                         Column('place_id', String(60), ForeignKey('places.id'),
+                                primary_key=True, nullable=False),
+                         Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                                primary_key=True, nullable=False)
+                         )
 
 
 class Amenity(BaseModel):
-    name = ""
+    """ Amenity class """
+    __tablename__ = 'amenities'
+    
+    name = Column(String(128), nullable=False)
+
+    # Establish the Many-To-Many relationship with Place
+    place_amenities = relationship("Place", secondary=association_table, back_populates="amenities")
