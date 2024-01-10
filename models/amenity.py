97495ee5077590d27fcd391f46c32654188626da
victormc13 <viktorsb14@gmail.com>
2024-01-10 14:11:35 -0800

    update: Updates the 'Amenity' class to represent 'amenities' table and 'place_amenities' class attribute for relationship between 'Place' and 'Amenity' classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_t
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Representation of Amenity"""
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
