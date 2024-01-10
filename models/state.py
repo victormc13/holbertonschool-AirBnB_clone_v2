f90d8a128f8814e902dfac158c9c11614da43dfb
victormc13 <viktorsb14@gmail.com>
2024-01-10 11:37:17 -0800

    update: Updates the 'State' class to inherit from 'BaseModel' and 'Base', and adds a relationship with 'cities'

--- models/state.py
+++ models/state.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_t
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    if storage_t == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City',
+                              backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a State instance"""
+        super().__init__(*args, **kwargs)
+
+    if storage_t != 'db':
+        @property
+        def cities(self):
+            """Getter for list of city instances related to the state"""
+            from models import storage
+            city_list = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+
+            return city_list
