84695b167551043fc26a5ae4fe9ad87ee422872b
victormc13 <viktorsb14@gmail.com>
2024-01-16 14:03:46 -0800

    new: Creates a new route '/hbnb_filters' that displays a HTML page with a filter for 'States' and 'Amenities'

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a Flash Web Application"""
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """Displays a HTML page with a list of states and amenities from storage"""
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+
+    return render_template('10-hbnb_filters.html',
+                           states=states,
+                           amenities=amenities)
+
+
+@app.teardown_appcontext
+def close_db_session(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
