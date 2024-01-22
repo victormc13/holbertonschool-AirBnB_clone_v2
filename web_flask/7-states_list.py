f05a49e49a863babe48d8a6bfbee663a1ae15277
victormc13 <viktorsb14@gmail.com>
2024-01-22 18:50:42 -0500

    update: Moves the 'close_db_session' at the end and use string literal instead of 'State' class for 'storage.all()' method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """Starts a Flash Web Application"""
 from models import storage
-from models.state import State
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays a HTML page with a list of states"""
+    states = storage.all('State').values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
+
+
 @app.teardown_appcontext
 def close_db_session(error):
     """Remove the current SQLAlchemy Session"""
     storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays a HTML page with a list of states"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
