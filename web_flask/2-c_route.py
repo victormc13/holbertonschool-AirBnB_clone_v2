b2619f6e591f34d8f4d9bbdc9a71fdd039676c74
victormc13 <viktorsb14@gmail.com>
2024-01-22 16:37:15 -0800

    update: fix pycodestyle pep8 coding style

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,15 +5,18 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays the greeting message 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """Displays 'HBNB'."""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
@@ -23,4 +26,3 @@ def display_c_text(text):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
